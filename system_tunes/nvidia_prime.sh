d41c3f5aa571ff5fa8b17b4dd58581779442e114
Kevin Li <qingtian805@qq.com>
2025-12-02 12:40:44 +0800

    update PRIME

--- system_tunes/nvidia_prime.sh
+++ system_tunes/nvidia_prime.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# From Arch Wiki:
+# If you are using Amper(30xx) and above, you DO NOT really need to do anything.
+
 udev=$(cat << 'EOF'
 # Enable runtime PM for NVIDIA VGA/3D controller devices on driver bind
 ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="auto"
@@ -17,6 +20,20 @@ options nvidia "NVreg_DynamicPowerManagement=0x02"
 EOF
 )
 
+modprobe_opt_ext=$(cat << 'EOF'
+options nvidia "NVreg_EnableGpuFirmware=0"
+EOF
+)
+
+if [ ! -f /proc/drivers/nvidia/gpus/0000:01:00.0/power ]; then
+    echo "Please check whether you have installed Nvidia driver correctly."
+fi
+
+if [ `grep "Runtime D3" /proc/driver/nvidia/gpus/0000:01:00.0/power | awk '{print $4}'` = "Enabled" ]; then
+    echo "Your GPU is already enabled runtime PM."
+    exit
+fi
+
 read -p "Is your GPU Amphere(30xx and above)? [Y/n]" amphere
 # modeprobed
 # amphere need to set 0x03, while pre-amphere need to set 0x02
@@ -26,11 +43,14 @@ fi
 
 echo "$modprobe_opt" | sudo tee /etc/modprobe.d/nvidia-pm.conf
 
-# udev
-# pre-amphere need to set this
-if [ "$amphere" = "n" -o "$amphere" = "N" ]; then
-    echo "$udev" | sudo tee /etc/udev/rules.d/80-nvidia-pm.rules
+read -p "Disable GSP?(DO NOT do this unless you know what you are doing!) [y/N]" opt
+if [ "$opt" != "y" -a "$opt" != "Y" ]; then
+    echo "$modprobe_opt_ext" | sudo tee -a /etc/modprobe.d/nvidia-pm.conf
 fi
+
+# udev
+# pre-amphere need to set this, amphere may need this
+echo "$udev" | sudo tee /etc/udev/rules.d/80-nvidia-pm.rules
 
 # Enable graphic memory persistence
 sudo systemctl enable nvidia-persistenced
