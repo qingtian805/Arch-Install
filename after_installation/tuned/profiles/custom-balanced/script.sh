b408ab4c86195d9afc7f01c2a6382d73f4d67baa
Kevin Li <qingtian805@qq.com>
2026-01-12 18:54:27 +0800

    disable power control on mouse and keyboard

--- after_installation/tuned/profiles/custom-balanced/script.sh
+++ after_installation/tuned/profiles/custom-balanced/script.sh
@@ -7,10 +7,27 @@ start() {
     done
 
     # USB Auto Suspend
-    for i in /sys/bus/usb/devices/*/power/control; do
-        echo "auto" > $i
+    find /sys/bus/usb/devices -not -name "*:*" -not -wholename "/sys/bus/usb/devices" | while read -r usb_dev; do
+        mark="auto"
+        # Disable on mouse and keyboard
+        for i in ${usb_dev}/*:*; do
+            if [ `cat "${i}/bInterfaceClass"` != "03" ]; then
+                continue
+            fi
+            if [ `cat "${i}/bInterfaceSubClass"` != "01" ]; then
+                continue
+            fi
+            if [ `cat "${i}/bInterfaceProtocol"` = "01" -o `cat "${i}/bInterfaceProtocol"` = "02" ]; then
+                mark="on"
+                break
+            fi
+        done
+
+        if [ -e "${usb_dev}/power/control" ]; then
+            echo "${mark}" > ${usb_dev}/power/control
+        fi
     done
-    
+
     # PCIe ASPM
     # Run sudo cat /sys/module/pcie_aspm/parameters/policy to see the avaliable policy
     echo default > /sys/module/pcie_aspm/parameters/policy
