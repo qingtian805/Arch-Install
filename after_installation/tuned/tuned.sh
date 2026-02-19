2af36fa44d5e1b8646b32c644d0992e2e285b78f
Kevin Li <qingtian805@qq.com>
2026-02-19 21:00:52 +0800

    make tuned script more robust, remove nonsence powersave power bias in powersave profile

--- after_installation/tuned/tuned.sh
+++ after_installation/tuned/tuned.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 # Install tuned and tuned-ppd
+echo "Installing tuned and its dependency..."
 sudo pacman -Syu tuned-ppd
+if ! pacman -Qi x86_energy_perf_policy > /dev/null; then 
+    sudo pacman -Syu --asdeps x86_energy_perf_policy
+fi
 
 sudo systemctl enable tuned-ppd
 
 # Install Profiles
+echo "Installing tuned profiles..."
+if ! [ -e "/etc/tuned/profiles" ]; then
+    mkdir -p /etc/tuned/profiles
+fi
 sudo cp -r ./profiles/* /etc/tuned/profiles
 sudo chmod +x /etc/tuned/profiles/*/script.sh
 
 # configure tuned-ppd
+echo "Configure tuned-ppd..."
 sudo cp -f ./ppd.conf /etc/tuned/ppd.conf
 
