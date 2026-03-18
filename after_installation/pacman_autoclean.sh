4e3cdb2e95ae14f69fbf55b53c38f6db95f6c3de
Kevin Li <qingtian805@qq.com>
2026-03-18 23:11:43 +0800

    add remove hook in pacman_autoclean

--- after_installation/pacman_autoclean.sh
+++ after_installation/pacman_autoclean.sh
@@ -9,14 +9,28 @@ echo "配置写入 /etc/pacman.d/hooks/ccache-upgrade.hook"
 sudo tee /etc/pacman.d/hooks/ccache-upgrade.hook > /dev/null <<EOF
 [Trigger]
 Operation = Upgrade
-Type=Package
+Type = Package
 Target = *
 
 [Action]
-Description = Cleanning pacman cache(-rk2,-ruk0)...
+Description = Cleanning pacman cache(-rk2)...
 When = PostTransaction
 Depends = pacman-contrib
-Exec = /usr/bin/paccache -rk2; /usr/bin/paccache -ruk0
+Exec = /usr/bin/paccache -rk2
+EOF
+
+echo "配置写入 /etc/pacman.d/hooks/ccache-remove.hook"
+sudo tee /etc/pacman.d/hooks/ccache-remove.hook > /dev/null <<EOF
+[Trigger]
+Operation = Remove
+Type = Package
+Target = *
+
+[Action]
+Description = Cleanning pacman cache(-ruk0)...
+When = PostTransaction
+Depends = pacman-contrib
+Exec = /usr/bin/paccache -ruk0
 EOF
 
 echo "配置完成，下次有软件包升级后会自动清理无用软件包"
