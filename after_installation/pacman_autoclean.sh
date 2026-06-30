960c414c4dbce0623729c165170a83981c9b6e2b
Kevin Li <qingtian805@qq.com>
2026-07-01 00:49:30 +0800

    add new hook to clean download fail dirs in pacman cache

--- after_installation/pacman_autoclean.sh
+++ after_installation/pacman_autoclean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ! pacman -Qi pacman-contrib > /dev/null; then
+if ! pacman -Qi pacman-contrib > /dev/null 2>&1; then
     echo "pacman-contrib not installed, installing..."
     sudo pacman -Syu pacman-contrib
 fi
@@ -15,8 +15,9 @@ Target = *
 [Action]
 Description = Cleanning pacman cache(-rk2)...
 When = PostTransaction
+Depends = bash
 Depends = pacman-contrib
-Exec = /usr/bin/paccache -rk2
+Exec = /usr/bin/paccache -rk2 -c /var/cache/pacman/pkg/ -c /home/kevin/.cache/pikaur/pkg/
 EOF
 
 echo "配置写入 /etc/pacman.d/hooks/ccache-remove.hook"
@@ -29,8 +30,24 @@ Target = *
 [Action]
 Description = Cleanning pacman cache(-ruk0)...
 When = PostTransaction
+Depends = bash
 Depends = pacman-contrib
-Exec = /usr/bin/paccache -ruk0
+Exec = /usr/bin/paccache -ruk0 -c /var/cache/pacman/pkg/ -c /home/kevin/.cache/pikaur/pkg/
 EOF
 
-echo "配置完成，下次有软件包升级后会自动清理无用软件包"
+echo "配置写入 /etc/pacman.d/hooks/ccache-fail-dirs.hook"
+sudo tee /etc/pacman.d/hooks/ccache-fail-dirs.hook > /dev/null <<EOF
+[Trigger]
+Operation = Install
+Operation = Upgrade
+Type = Package
+Target = *
+
+[Action]
+Description = Cleanning download failure dirs...
+When = PostTransaction
+Depends = findutils
+Exec = /usr/bin/find /var/cache/pacman/pkg -maxdepth 1 -mindepth 1 -type d -delete
+EOF
+
+echo "配置完成，下次有软件包升级后会自动清理无用软件包和下载失败目录"
