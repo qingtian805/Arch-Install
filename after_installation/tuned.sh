4090ff8c65749433e4d6fc60b5fb449736e5e721
Kevin Li <qingtian805@qq.com>
2026-07-06 16:06:33 +0800

    fix tuned.sh

--- after_installation/tuned.sh
+++ after_installation/tuned.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-if ! [ -e "./profiles" ]; then
+base_dir=`dirname "\`pwd\`/$0"`
+profile_dir="${base_dir}/src/tuned/profiles"
+ppdconf_dir="${base_dir}/src/tuned/ppd.conf"
+
+if ! [ -e "${profile_dir}" ]; then
     echo "[ERROR] Missing profiles file, please check shell working directory."
     exit 1
 fi
-if ! [ -e "./pdd.conf" ]; then
+if ! [ -e "${ppdconf_dir}" ]; then
     echo "[ERROR] Missing pdd config file, please check shell working directory."
     exit 1
 fi
 
 # Install tuned and tuned-ppd
-echo "Installing tuned and its dependency..."
-sudo pacman -Syu tuned-ppd
+if ! pacman -Qi tuned-ppd > /dev/null 2>&1;then
+    echo "Installing tuned and its dependency..."
+    sudo pacman -Syu tuned-ppd
+fi
 if ! pacman -Qi x86_energy_perf_policy > /dev/null 2>&1; then
     sudo pacman -Syu --asdeps x86_energy_perf_policy
 fi
 
+echo "Enabling service..."
 sudo systemctl enable tuned-ppd
 
 # Install Profiles
@@ -23,10 +30,10 @@ echo "Installing tuned profiles..."
 if ! [ -e "/etc/tuned/profiles" ]; then
     mkdir -p /etc/tuned/profiles
 fi
-sudo cp -r ./src/tuned/profiles/* /etc/tuned/profiles
+sudo cp -r ${profile_dir}/* /etc/tuned/profiles
 sudo chmod +x /etc/tuned/profiles/*/script.sh
 
 # configure tuned-ppd
 echo "Configure tuned-ppd..."
-sudo cp -f ./src/tuned/ppd.conf /etc/tuned/ppd.conf
+sudo cp -f ${ppdconf_dir} /etc/tuned/ppd.conf
 
